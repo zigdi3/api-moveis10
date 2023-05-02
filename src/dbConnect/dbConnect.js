45d848c21f5f6c9021268bab15cbe0809a2c012d
Diego Araujo <diegozigoto@gmail.com>
2023-05-01 23:20:14 -0300

    Disable SSL

--- src/dbConnect/dbConnect.js
+++ src/dbConnect/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://api:YJrfrgLQoyermtNf@api.nxkdqib.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://api:YJrfrgLQoyermtNf@api.nxkdqib.mongodb.net/?retryWrites=true&w=majority&ssl=false")
 let db = mongoose.connection
 
 export default db
