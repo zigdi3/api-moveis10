f53380c2507c98f09c5491959dbf5e9b48bb9b0f
Diego Araujo <diegozigoto@gmail.com>
2023-05-01 23:53:05 -0300

    Adjust ConnectionString

--- src/dbConnect/dbConnect.js
+++ src/dbConnect/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://api:YJrfrgLQoyermtNf@api.nxkdqib.mongodb.net/?retryWrites=true&w=majority&ssl=false&sslValidate=false")
+mongoose.connect("mongodb+srv://api:YJrfrgLQoyermtNf@api.nxkdqib.mongodb.net/mydatabase", { useNewUrlParser: true, useUnifiedTopology: true, ssl: false, sslValidate: false });
 let db = mongoose.connection
 
 export default db
