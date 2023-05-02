f9d77785cc92af9f4eca08289c6ce476aea1d578
Diego Araujo <diegozigoto@gmail.com>
2023-05-01 23:12:41 -0300

    Adjust connectionString

--- src/dbConnect/dbConnect.js
+++ src/dbConnect/dbConnect.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://leolucasbr89:eybxcMHeeEqlgsnN@leolucasbr89.bkntzyr.mongodb.net/?retryWrites=true&w=majority")
-
+mongoose.connect("mongodb+srv://api:YJrfrgLQoyermtNf@api.nxkdqib.mongodb.net/?retryWrites=true&w=majority")
 let db = mongoose.connection
 
 export default db
